dfc7532cc2ac3029b7cd3b095ee9a3932504a804
Julius Palalay <julius.palalay@usjr.edu.ph>
2022-12-23 21:17:35 +0800

    commit

---
+++ src/components/Checkout/Checkout.tsx
@@ -0,0 +1,34 @@
+import { useContext, useState } from "react";
+import { Product } from "../../models";
+import { CheckoutCard } from "../CheckoutCard";
+
+import { ShopContext } from "../Context/useContext";
+import { Labels, PriceContainer, ProductsWrapper, Title, Total, TotalAmount, TotalAmountLabel, TotalItems, TotalItemsLabel } from "./Checkout.styled";
+
+export const Checkout = () => {
+  const { products, total, totalitems } = useContext(ShopContext);
+ 
+
+
+
+
+  return (
+    <>
+    <Title>Checkout</Title>
+    <ProductsWrapper>
+        {products.map((product: Product, index) => (
+          <CheckoutCard {...product} key={index} />
+        ))}
+      </ProductsWrapper>
+      <Total>
+      <Labels>
+          <TotalAmountLabel>Total Amount:</TotalAmountLabel>
+        </Labels>
+        <PriceContainer>
+          <TotalAmount>${total}.00</TotalAmount>
+        </PriceContainer>
+      </Total>
+
+    </>
+  );
+};
